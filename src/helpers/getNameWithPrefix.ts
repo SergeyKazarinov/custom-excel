b39abb2e5dda360a4d165393798d69be50aaa23c
Sergey Kazarinov <kazarinov092@gmail.com>
2024-08-10 14:14:02 +0500

    refact: move getNameWithPrefix from DomListener to helpers

---
+++ src/helpers/getNameWithPrefix.ts
@@ -0,0 +1,12 @@
+import { TListeners, TMethods } from '@src/types/listeners';
+import capitalize from './capitalize';
+
+/**
+ * Переводит полученную строку в camelCase формат с префиксом 'on'
+ *
+ * @param {TListeners} eventName - название слушателя
+ * @returns {TMethods} - - строку с префиксом on в формате camelCase
+ */
+const getNameWithPrefix = (eventName: TListeners): TMethods => `on${capitalize(eventName)}`;
+
+export default getNameWithPrefix;
