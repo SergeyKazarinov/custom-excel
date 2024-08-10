41729d9155a105c5c347e09a8e9c50564403acc5
Sergey Kazarinov <kazarinov092@gmail.com>
2024-08-10 16:39:34 +0500

    docs: refact ExcelStateComponent and add TS-doc

--- src/core/excelStateComponent/ExcelStateComponent.types.ts
+++ src/core/excelStateComponent/ExcelStateComponent.types.ts
@@ -1,6 +1,23 @@
+export interface IExcelStateComponent<S> {
+  /**
+   * Инициализация стора
+   * @param initialState - начальное состояния store
+   */
+  initState(initialState: S): void;
+
+  /**
+   * Установка нового состояния
+   *
+   * @param {S} newState
+   */
+  setState(newState: S): void;
+}
+
 export interface IToolbarState {
   textAlign: 'left';
   fontWeight: 'normal';
   textDecoration: 'none';
   fontStyle: 'normal';
 }
+
+export type State = { [key: string]: any };
