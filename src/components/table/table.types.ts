70813499f354c8f8c0476069159dac75903a9d81
Sergey Kazarinov <kazarinov092@gmail.com>
2024-08-13 00:10:22 +0500

    docs: add ts-doc for Tabler

--- src/components/table/table.types.ts
+++ src/components/table/table.types.ts
@@ -1,5 +1,65 @@
+import { Dom } from '@src/core/dom/dom';
 import { IRootState } from '@src/store/store.types';
+import { IInputEvent } from '@src/types/general';
 
+export interface ITable {
+  /**
+   * Рендер таблицы в dom-дерево
+   *
+   * @returns {string} - html-элемент таблицы
+   */
+  toHTML(): string;
+
+  /**
+   * Выделение ячейки таблицы
+   *
+   * @param {Dom} $cell - ячейка, на которой произошел focus
+   */
+  selectCell($cell: Dom): void;
+
+  /**
+   * Изменение ширины/высоты таблицы
+   *
+   * @param {MouseEvent} event - строка или столбец, на котором происходит изменение свойства
+   * @returns {Promise<void>}
+   */
+  resizeTable(event: MouseEvent): Promise<void>;
+
+  /**
+   * Инициализация таблицы:
+   * фокусировка ячейки
+   * установка слушателей событий
+   */
+  init(): void;
+
+  /**
+   * Событие нажатие мыши элемента
+   *
+   * @type {(event: MouseEvent) => void} - элемент, на котором произошло событие
+   */
+  onMousedown: (event: MouseEvent) => void;
+
+  /**
+   * выделяет несколько ячеек, если нажата клавиша shift
+   *
+   * @type {(event: KeyboardEvent) => void} - ячейка, на которую нажали с клавишой shift
+   */
+  onKeydown: (event: KeyboardEvent) => void;
+
+  /**
+   * Изменение текста на текущей ячейке и сохранение в store
+   *
+   * @param {string} text - обновленный текст
+   */
+  updateCurrentTextInStore(text: string): void;
+
+  /**
+   * Изменение текста при изменении содержимого ячейки и сохранение в store
+   *
+   * @param {IInputEvent} event - ячейка, на которой происходит событие
+   */
+  onInput(event: IInputEvent): void;
+}
 export interface ICreateTable {
   rowsCount?: number;
   state: IRootState;
